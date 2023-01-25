d93d9eaacff4e2a6cb5794cb5e9d9a7d9ff849b9
Yuliya <i@yzubkova.ru>
2023-01-25 18:13:45 +0300

    Программа считает количество элементов массива, лежащих в заданном диапазоне

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения для которых 
+// лежат в отрезке (10-99).
+
+//Инициализирует новый массив
+int[] InitArray(int length)
+{
+    int[] array = new int [length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(-100, 300);
+    }
+
+    return array;
+}
+
+// Печатает массив на экран
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+// Считает количество элементов массива в заданном диапазоне
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+     {
+     if (array[i] >= 10 && array[i] <= 99) count++;
+     }
+     return count;
+    
+}
+
+Console.WriteLine("Введите размер массива ");
+int [] array = InitArray(int.Parse(Console.ReadLine()));
+
+PrintArray(array);
+Count(array);
+
+Console.WriteLine($"Количество элементов массива в заданном диапазоне: {Count(array)}");

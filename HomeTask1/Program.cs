b4f107edee011b4e47bc44d1b9b321d83144bbd1
Yuliya <i@yzubkova.ru>
2023-01-26 07:18:40 +0300

    Программа показывает количество четных чисел в массиве трехзначных чисел

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -1,6 +1,34 @@
-﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
 
 [345, 897, 568, 234] -> 2
-*/ 
+*/
 
-Console.WriteLine("Hello, World!");
+
+int[] InitArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+
+    return array;
+}
+
+int PrintArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) sum = sum + 1;
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Количество четных чисел в массиве: {sum} ");
+    return sum;
+}
+
+int[] arr = InitArray(4);
+PrintArray(arr);

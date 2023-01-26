675dd1e96d2e6f8d0a09578e110ffc6c1fba1f86
Yuliya <i@yzubkova.ru>
2023-01-26 08:55:32 +0300

    Вычисление разницы между максимальным и минимальным элементом массива

--- HomeTask3/Program.cs
+++ HomeTask3/Program.cs
@@ -1,5 +1,57 @@
-﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+﻿/* Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
 
 [3 7 22 2 78] -> 76*/
 
-Console.WriteLine("Hello, World!");
+double [] InitArray(int length)
+{
+    double[] array = new double [length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(1,100);
+    }
+
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+double GetMax(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    return max;
+}
+
+double GetMin(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    return min;
+}
+
+double[] arr = InitArray(5);
+PrintArray(arr);
+
+double max = GetMax(arr);
+double min = GetMin(arr);
+
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {max-min}");

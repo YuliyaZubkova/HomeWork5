e49c0eed5803f19728a09e3b060b697e5be1a59a
Yuliya <i@yzubkova.ru>
2023-02-09 11:50:46 +0300

    Находит произведение чисел первого и последнего и т.д. в одномерном массиве

---
+++ 1111/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+*/
+
+int[] InitArray()
+{
+    int[] result = new int[15];
+    Random rnd = new Random();
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(-100,200);
+    }
+
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int Count(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] < 100) count++;
+    }
+    return count;
+}
+
+
+
+int [] arr = InitArray();
+PrintArray(arr);
+Count(arr);
+
+Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10, 99] равно {Count(arr)}");
--- Task5/Program.cs
+++ Task5/Program.cs
@@ -2,82 +2,45 @@
 // Пару считаем 1+последнее число, 2+ предпоследнее...
 
 
-/*Инициализирует новый массив
-int[] InitArray(int length)
+int[] InitArray(int dimension)
 {
+    int[] result = new int[dimension];
     Random rnd = new Random();
-    int[] array = new int [length];
-    for (int i = 0; i < length; i++)
+
+    for (int i = 0; i < result.Length; i++)
     {
-        array[i] = rnd.Next(1, 5);
+        result[i] = rnd.Next(2,7);
     }
 
-    return array;
+    return result;
 }
 
-
-int[] NewArray(int[] array)
+void PrintArray(int[] arr)
 {
-    int[] arr; 
-    int len = 0;
-    
-    if (array.Length%2 == 0) 
+    for (int i = 0; i < arr.Length; i++)
     {
-        len = array.Length/2;
-        arr = new int[len];
-    }
-    else 
-    {
-        len = array.Length/2 + 1;
-        arr = new int [len];
-        arr[len-1] = array[len];
-
-    }
-
-    for (int i = 0; i < len; i++) // непонятно как сделать чтобы середина не умножалась на 2
-    {
-        arr[i] = array[i]*array[array.Length - i - 1];
-    }
-    return arr;
-}
-
-// Печатает массив на экран
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]}, ");
+        Console.Write($"{arr[i]} ");
     }
     Console.WriteLine();
 }
 
-Console.WriteLine("Введите размер массива ");
-int [] array = InitArray(int.Parse(Console.ReadLine()));
+int[] GetChange(int[] array)
+{
+    int[] result; 
+if (array.Length %2 ==0)
+result = new int [array.Length/2];
+else result = new int [array.Length/2+1];
+
+for (int i = 0; i < array.Length/2; i++)
+{
+    result[i] = array[i] * array[array.Length-1-i];
+}
+if (array.Length %2 !=0) 
+result[result.Length-1] = array[array.Length/2];
+return result;
+}
+
+var array = InitArray(5);
 PrintArray(array);
-int [] arr = NewArray(array);
-Console.WriteLine();
-PrintArray(arr);
-*/
-
-
-  static class func 
-  { 
-    static void Main() 
-    { 
-      Console.WriteLine("Размер массива: "); 
-      int n; 
-      n = Convert.ToInt32(Console.ReadLine()); 
-      int[] ar = new int[n]; 
-      var rand = new Random(); 
-       
-      for(int i = 0; i < ar.Length; i++) 
-        ar[i] = rand.Next(-50, 50); 
-      Console.Write("Значения элементов массива: "); 
-      foreach(var item in ar) 
-        Console.Write("{0} ", item); 
-      Console.WriteLine(); 
-      for(int i = 0; i < ar.Length / 2; i++) 
-        Console.WriteLine("элемент {0} х элемент {1}, {2} х {3}  = {4}", i, ar.Length - i - 1, ar[i], ar[ar.Length - i - 1], ar[i] * ar[ar.Length - i - 1]); 
-      return; 
-    } 
-  } 
+var result = GetChange(array);
+PrintArray(result);
